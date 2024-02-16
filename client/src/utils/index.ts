f296aeaece08bfa20319909ed19076d36eefa5a6
artemkolodko <temakolodko@gmail.com>
2024-02-16 18:47:37 +0000

    Fix warnings

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,14 +1,14 @@
 export * from './getAddress/GetAddress'
 
-export const shortenAddress = (address: string): string => {
-  const start = address.substring(0, 4 + 2); // +2 to include '0x'
-  const end = address.substring(address.length - 4);
-  return start; // `${start}...${end}`;
-}
-
-export const capitalizeString = (str: string) => {
-  if(str.length === 0) {
-    return ''
-  }
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
+// export const shortenAddress = (address: string): string => {
+//   const start = address.substring(0, 4 + 2); // +2 to include '0x'
+//   const end = address.substring(address.length - 4);
+//   return start; // `${start}...${end}`;
+// }
+//
+// export const capitalizeString = (str: string) => {
+//   if(str.length === 0) {
+//     return ''
+//   }
+//   return str.charAt(0).toUpperCase() + str.slice(1);
+// }
