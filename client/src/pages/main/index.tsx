b3890aabff1361f71dd4b8f3fc81894608d057c9
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2024-02-16 13:27:44 -0800

    redirect index to user page

--- client/src/pages/main/index.tsx
+++ client/src/pages/main/index.tsx
@@ -1,15 +1,25 @@
 import React, {useEffect, useState} from "react";
 import {Box, Spinner, Text} from "grommet";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import '../../index.css'
 import {Action} from "../../types";
 import {UserAction} from "../../components/action";
 import {getMessages} from "../../api/firebase";
+import { useUserContext } from "../../context/UserContext";
 
 export const MainPage = () => {
+  const { wallet } = useUserContext();
+  const navigate = useNavigate();
   const { key } = useParams();
   const [actions, setActions] = useState<Action[]>([]);
   const [isLoading, setLoading] = useState(false)
+
+  // NOTE: for now, we will redirect all the users to their own page
+  useEffect(() => {
+    if (wallet && wallet.address) {
+      navigate(`/0/${wallet.address.substring(2)}`);
+    }
+  }, [wallet, navigate]);
 
   useEffect(() => {
     const loadData = async () => {
