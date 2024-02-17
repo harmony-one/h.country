06fcec494868170832d4a2bf1c16455a97dc9627
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2024-02-16 17:02:17 -0800

    remove unused definition

--- client/src/components/menu/index.tsx
+++ client/src/components/menu/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box, Text } from "grommet"
 import { useUserContext } from '../../context/UserContext'
-import {useNavigate, useParams} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const AppMenu = () => {
   const navigate = useNavigate()
