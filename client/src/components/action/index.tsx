e7283545654ac9358b7944098394c920338feb7b
fegloff <fegloff@gmail.com>
2024-02-19 16:41:04 -0500

    add hash colors for self, other and verified actions (verified to be completed)

--- client/src/components/action/index.tsx
+++ client/src/components/action/index.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Text } from "grommet";
 import { Link } from "react-router-dom";
 import moment from 'moment'
 import { Action } from "../../types";
 import { socialUrlParser, formatAddress } from "../../utils";
+import { useUserContext } from "../../context/UserContext";
+import styled from "styled-components";
+
+type ActionType = 'self' | 'other' | 'verified'
+const handleActionType = (type: ActionType) => {
+  switch (type) {
+    case 'verified':
+      return "#a0ffa0 !important"
+    case 'other':
+      return "#FFF7AD !important";
+    default:
+      return "#64ebfd"; // self
+  }
+};
+
+const ActionLink = styled(Link)<{ type?: ActionType }>`
+  :visited, :link, :hover, :active {
+    color: ${(props) => props.type && handleActionType(props.type)};
+  }
+  text-decoration: none;
+`;
 
 export interface UserActionProps {
   action: Action
@@ -12,7 +33,19 @@ export interface UserActionProps {
 
 export const UserAction = (props: UserActionProps) => {
   const { action } = props
-
+  const { wallet } = useUserContext();
+  const [actionType, setActionType] = useState<ActionType>('self')
+  
+  console.log('ADDRESS', wallet)
+  useEffect(() => {
+    const myAddress = wallet?.address.slice(2);
+    if (myAddress === action.from) {
+      setActionType('self')
+    } else {
+      setActionType('other')
+    }
+  }, [])
+  
   const onTagClicked = () => {
     if (props.onTagClicked && action.payload) {
       props.onTagClicked(action.payload)
@@ -31,7 +64,7 @@ export const UserAction = (props: UserActionProps) => {
       <Box direction={'row'} justify={'start'} pad={'0 16px'}>
         <Box basis="50%">
           <Text size={"small"} style={{ wordBreak: 'break-all' }}>
-            <Link className="link" to={`/0/${action.from}`}>0/{action.fromShort}</Link>
+            <Link className="link" to={`/0/${action.from}`} style={{ }}>0/{action.fromShort}</Link>
             {" tags "}
             <Text size={"small"} onClick={onTagClicked}>#{action.payload}</Text>
             {" on "}
@@ -53,14 +86,14 @@ export const UserAction = (props: UserActionProps) => {
       <Box direction={'row'} justify={'start'} pad={'0 16px'}>
         <Box basis="50%">
           <Text size={"small"} style={{ wordBreak: 'break-all' }}>
-            <Link className="link" to={`/0/${action.from}`}>0/{action.fromShort}</Link>
+            <ActionLink className="link" to={`/0/${action.from}`} type={actionType}>0/{action.fromShort}</ActionLink>
             {" links "}
             <Text size={"small"}>{socialUrlParser(action.payload || '')[0]?.name}</Text>
             {' '}
-            <Link className="link" to={`/0/${action.from}`}>{
+            <ActionLink className="link" to={`/0/${action.from}`} type={actionType}>{
               socialUrlParser(action.payload || '')[0]?.type + '/' + socialUrlParser(action.payload || '')[0]?.username
             }
-            </Link>
+            </ActionLink>
           </Text>
         </Box>
         <Box align={'end'} basis="40%" style={{ minWidth: '32px' }}>
