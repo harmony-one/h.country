ed591551cc4a66645f3eb45946e4f79d190ca6bf
artemkolodko <temakolodko@gmail.com>
2024-02-19 09:31:07 +0000

    Move moment.js config to separate file

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,28 +1,6 @@
 import oneColor from './assets/logos/one-color.svg'
 import oneOutline from './assets/logos/one-grey.svg'
 import {type UserTopic, UserTopicType} from "./types";
-import moment from 'moment'
-
-moment.updateLocale('en', {
-  relativeTime : {
-    future: "in %s",
-    past:   "%s", // "%s ago"
-    s  : 'few s',
-    ss : '%ds',
-    m:  "1m",
-    mm: "%dm",
-    h:  "1h", // 1 hour ago
-    hh: "%dh",
-    d:  "1d",
-    dd: "%dd",
-    w:  "1w",
-    ww: "%dw",
-    M:  "1m", //change this for month
-    MM: "%dm",
-    y:  "1y",
-    yy: "%dy"
-  }
-});
 
 export const baseTopicList = [
   { name: 'chainlink', group: 1, type: 'blockchain'},
