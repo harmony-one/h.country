d79747d9a3b5bcbef1c10285e6b1572606010cdd
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2024-02-21 14:53:08 -0800

    /new update navbar to h.country and copy private key

--- client/src/components/menu/index.tsx
+++ client/src/components/menu/index.tsx
@@ -1,22 +1,36 @@
 import React from 'react'
 import { Box, Text } from "grommet"
 import { useUserContext } from '../../context/UserContext'
-import { useNavigate } from "react-router-dom";
 import { LocationButton } from './LocationButton';
 
 export const AppMenu = () => {
-  const navigate = useNavigate()
   const { wallet } = useUserContext();
-
+  const handleCopyPrivateKey = () => {
+    const privateKey = wallet!.privateKey!.substring(2);
+  
+    navigator.clipboard.writeText(privateKey).then(() => {
+      alert("copied key to clipboard");
+    }).catch(_ => {
+      // fallback
+      const textarea = document.createElement('textarea');
+      textarea.value = privateKey;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+      alert("copied key to clipboard");
+    });
+  };
+  
   return <Box>
     <Box direction={'row'} justify={'between'}>
       <Box>
         <LocationButton />
       </Box>
       <Box flex="grow" />
-      <Box onClick={() => navigate(`/0/${wallet?.address.replace('0x', '')}`)}>
+      <Box onClick={handleCopyPrivateKey}>
         <Text color={"blue1"}>
-          0/{wallet?.address.substring(2, 6)}
+          h.country
         </Text>
       </Box>
     </Box>
