8cca348c5968940a1d65acd5d7ceabff7a142eb5
fegloff <fegloff@gmail.com>
2024-02-21 14:50:20 -0500

    udpate filter panel on UserPage route component

--- client/src/pages/user/UserPage.tsx
+++ client/src/pages/user/UserPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { Box, Button, Spinner, Text } from "grommet";
-import { PlainButton } from "../../components/button";
+import { PlainButton, PlainText } from "../../components/button";
 import { useUserContext } from "../../context/UserContext";
 import { collection, doc, onSnapshot, query, where } from "firebase/firestore";
 import { db } from "../../configs/firebase-config";
@@ -12,10 +12,12 @@ import styled from "styled-components";
 import { useSearchParams } from "react-router-dom";
 import { addMessageWithGeolocation } from "../../api";
 import { useActionsContext } from "../../context";
+import { FireOutlined, HeartOutlined, StarOutlined } from "@ant-design/icons";
 
 const HeaderText = styled(Text)`
   font-size: min(1em, 3vw);
-`
+`;
+
 const SmallHeaderText = styled(Text)`
   font-size: min(0.8em, 2.5vw);
   line-height: 2.3em;
@@ -27,16 +29,28 @@ const SmallHeaderText = styled(Text)`
   @media only screen and (min-width: 450px) {
     line-height: 1em;
   }
-`
+`;
+
+const UserPageBox = styled(Box)`
+  .filter-panel {
+    margin-top: 10px;
+    margin-bottom: 5px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+  }
+`;
 
 const predefinedLinks = [
-  { key: 'x', displayText: 'Twitter' },
-  { key: 'ig', displayText: 'Instagram' },
-  { key: 'g', displayText: 'Github' },
-  { key: 'f', displayText: 'Facebook' },
-  { key: 'l', displayText: 'LinkedIn' },
-  { key: 't', displayText: 'Telegram' },
-  { key: 's', displayText: 'Substack' },
+  { key: "x", displayText: "Twitter" },
+  { key: "ig", displayText: "Instagram" },
+  { key: "g", displayText: "Github" },
+  { key: "f", displayText: "Facebook" },
+  { key: "l", displayText: "LinkedIn" },
+  { key: "t", displayText: "Telegram" },
+  { key: "s", displayText: "Substack" },
 ];
 
 interface LinkItem {
@@ -57,18 +71,18 @@ interface Message {
 }
 
 const parseTagsFromUrl = (hashtagList: string): [string, number][] => {
-  const topics = hashtagList.split(',')
-  return topics.map(topic => {
-    const [tag, counter = '1'] = topic.split('^')
-    return [
-      tag,
-      Number(counter) || 0
-    ]
-  })
-}
+  const topics = hashtagList.split(",");
+  return topics.map((topic) => {
+    const [tag, counter = "1"] = topic.split("^");
+    return [tag, Number(counter) || 0];
+  });
+};
 
 function isHex(num: string): Boolean {
-  return Boolean(num.match(/^0x[0-9a-f]+$/i)) || Boolean(`0x${num}`.match(/^0x[0-9a-f]+$/i))
+  return (
+    Boolean(num.match(/^0x[0-9a-f]+$/i)) ||
+    Boolean(`0x${num}`.match(/^0x[0-9a-f]+$/i))
+  );
 }
 export const UserPage = (props: { id: string }) => {
   const { wallet } = useUserContext();
@@ -77,7 +91,7 @@ export const UserPage = (props: { id: string }) => {
   const [isUserPage, setIsUserPage] = useState(false);
   const [tagItems, setTagItems] = useState<TagItem[]>([]);
   const [searchParams] = useSearchParams();
-  const topicsQueryParam = searchParams.get('topics')
+  const topicsQueryParam = searchParams.get("topics");
   const {
     actions,
     filters,
@@ -85,7 +99,7 @@ export const UserPage = (props: { id: string }) => {
     filterMode,
     setFilterMode,
     DefaultFilterMode,
-    isLoading
+    isLoading,
   } = useActionsContext();
 
   useEffect(() => {
@@ -95,40 +109,49 @@ export const UserPage = (props: { id: string }) => {
       if (docSnap.exists()) {
         const data = docSnap.data();
 
-        let linkItems: LinkItem[] = predefinedLinks.map(({ key, displayText }) => {
-          // Check if the key exists in the fetched data
-          if (data[key] && data[key].username && data[key].url) {
-            return {
-              id: docSnap.id + key,
-              text: (
-                <a href={data[key].url} target="_blank" rel="noopener noreferrer" style={{ color: 'white', textDecoration: 'none' }}>
-                  {`${key}/${data[key].username}`}
-                </a>
-              ),
-              predefined: false,
-              provider: displayText
-            };
-          } else {
-            // Return a default link item with the display text if the key does not exist
-            return {
-              id: docSnap.id + key,
-              text: <Text>{displayText}</Text>,
-              predefined: true,
-              provider: displayText
-            };
+        let linkItems: LinkItem[] = predefinedLinks.map(
+          ({ key, displayText }) => {
+            // Check if the key exists in the fetched data
+            if (data[key] && data[key].username && data[key].url) {
+              return {
+                id: docSnap.id + key,
+                text: (
+                  <a
+                    href={data[key].url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ color: "white", textDecoration: "none" }}
+                  >
+                    {`${key}/${data[key].username}`}
+                  </a>
+                ),
+                predefined: false,
+                provider: displayText,
+              };
+            } else {
+              // Return a default link item with the display text if the key does not exist
+              return {
+                id: docSnap.id + key,
+                text: <Text>{displayText}</Text>,
+                predefined: true,
+                provider: displayText,
+              };
+            }
           }
-        });
+        );
 
         setUrls(linkItems);
       } else {
         console.log("No such document!");
         // for other users (isUserPage == false)
-        setUrls(predefinedLinks.map(({ key, displayText }) => ({
-          id: 'default' + key,
-          text: <Text>{displayText}</Text>,
-          predefined: true,
-          provider: displayText
-        })));
+        setUrls(
+          predefinedLinks.map(({ key, displayText }) => ({
+            id: "default" + key,
+            text: <HeaderText>{displayText}</HeaderText>,
+            predefined: true,
+            provider: displayText,
+          }))
+        );
       }
     });
 
@@ -162,34 +185,45 @@ export const UserPage = (props: { id: string }) => {
         {}
       );
 
-      const tagsFromUrl = topicsQueryParam ? parseTagsFromUrl(topicsQueryParam || '') : []
+      const tagsFromUrl = topicsQueryParam
+        ? parseTagsFromUrl(topicsQueryParam || "")
+        : [];
 
-      const tagsList = tagsFromUrl.length ? tagsFromUrl : Object.entries(hashtagFrequency)
+      const tagsList = tagsFromUrl.length
+        ? tagsFromUrl
+        : Object.entries(hashtagFrequency);
 
-      const sortedHashtags = tagsList.filter(item => item[0] !== '')
+      const sortedHashtags = tagsList
+        .filter((item) => item[0] !== "")
         .sort((a, b) => b[1] - a[1])
         .slice(0, 9)
         .map(([hashtag, count]) => ({
           id: hashtag, // Use hashtag as a unique ID
           text: (
-            <Button onClick={async (e) => {
-              e.preventDefault();
-              if (wallet !== undefined && key !== undefined) {
-                const addressWithoutPrefix = wallet.address.slice(2);
-                await addMessageWithGeolocation(addressWithoutPrefix, `#${hashtag} @${key}`);
-              } else {
-                console.log("Invalid user wallet");
-              }
-            }}
-              plain>
+            <Button
+              onClick={async (e) => {
+                e.preventDefault();
+                if (wallet !== undefined && key !== undefined) {
+                  const addressWithoutPrefix = wallet.address.slice(2);
+                  await addMessageWithGeolocation(
+                    addressWithoutPrefix,
+                    `#${hashtag} @${key}`
+                  );
+                } else {
+                  console.log("Invalid user wallet");
+                }
+              }}
+              plain
+            >
               <Box direction={"row"}>
-                <HeaderText>{isHex(hashtag) ? `0/${hashtag.substring(0, 4)}` : hashtag}</HeaderText>
+                <HeaderText>
+                  {isHex(hashtag) ? `0/${hashtag.substring(0, 4)}` : hashtag}
+                </HeaderText>
                 <SmallHeaderText>{count}</SmallHeaderText>
               </Box>
             </Button>
-          )
-        })
-        );
+          ),
+        }));
 
       setTagItems(sortedHashtags);
     });
@@ -199,122 +233,183 @@ export const UserPage = (props: { id: string }) => {
 
   const extendedUrls = useMemo<LinkItem[]>(() => {
     const latestLocation = actions.find(
-      a => a.from === wallet?.address.slice(2) && !!a.address.road
+      (a) => a.from === wallet?.address.slice(2) && !!a.address.road
     )?.address;
 
     if (!latestLocation?.road) {
       return urls;
     }
 
-    return [{
-      id: 'latest_location' + latestLocation?.postcode,
-      text: (
-        <a
-          href={linkToMapByAddress(latestLocation)}
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: 'white', textDecoration: 'none' }}
-        >
-          {`m/${latestLocation?.short || formatAddress(latestLocation?.road)}`}
-        </a>
-      )
-    }, ...urls]
-  }, [actions, urls, wallet?.address])
+    return [
+      {
+        id: "latest_location" + latestLocation?.postcode,
+        text: (
+          <a
+            href={linkToMapByAddress(latestLocation)}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            {`m/${
+              latestLocation?.short || formatAddress(latestLocation?.road)
+            }`}
+          </a>
+        ),
+      },
+      ...urls,
+    ];
+  }, [actions, urls, wallet?.address]);
 
   if (!key || !isValidAddress(key)) {
     return <Box>Not a valid user ID</Box>;
   }
 
   const onTagClicked = (hashtag: string) => {
-    if (!filters.find(item => item.value === hashtag)) {
-      setFilters([...filters, {
-        type: 'hashtag',
-        value: hashtag
-      }])
-      setFilterMode('hashtag')
+    if (!filters.find((item) => item.value === hashtag)) {
+      setFilters([
+        ...filters,
+        {
+          type: "hashtag",
+          value: hashtag,
+        },
+      ]);
+      setFilterMode("hashtag");
     }
-  }
+  };
 
   const onLocationClicked = (location: string) => {
-    if (!filters.find(item => item.value === location)) {
-      setFilters([...filters, {
-        type: 'location',
-        value: location
-      }])
-      setFilterMode('location')
+    if (!filters.find((item) => item.value === location)) {
+      setFilters([
+        ...filters,
+        {
+          type: "location",
+          value: location,
+        },
+      ]);
+      setFilterMode("location");
     }
-  }
+  };
 
   const headerListProps = {
     userId: key,
     isLoading,
     isUserPage,
-    wallet
-  }
+    wallet,
+  };
 
   return (
-    <Box>
-      <Box gap={'16px'} pad={'0 16px'}>
-        <HeaderList
-          {...headerListProps}
-          type={"url"}
-          items={extendedUrls}
-        />
-        <HeaderList
-          {...headerListProps}
-          type={"hashtag"}
-          items={tagItems}
-        />
+    <UserPageBox>
+      <Box gap={"16px"} pad={"0 16px"}>
+        <HeaderList {...headerListProps} type={"url"} items={extendedUrls} />
+        <HeaderList {...headerListProps} type={"hashtag"} items={tagItems} />
       </Box>
-      <Box pad={'0 16px'}>
-        <Box direction={"row"} gap={"16px"}>
+      <div className="filter-panel">
+        <Box direction={"row"}>
           <PlainButton
             isActive={filterMode === "all"}
             onClick={() => setFilterMode("all")}
           >
-            All
+            <PlainText fontSize="min(1em, 4vw)">all</PlainText>
           </PlainButton>
           <PlainButton
             isActive={filterMode === "address"}
             onClick={() => setFilterMode("address")}
+            fontColor={isUserPage ? "blue1" : "yellow1"}
           >
-            <Text color={isUserPage ? "blue1" : "yellow1"}>
-              {key?.substring(0, 4)}
-            </Text>
-          </PlainButton>
-          {filters.map(filter => {
-            const { value } = filter
-            const onClick = () => {
-              const newFilters = filters.filter(item => item.value !== value)
-              setFilters(newFilters)
-              if (newFilters.length === 0) {
-                setFilterMode(DefaultFilterMode)
-              }
-            }
-
-            return <PlainButton
-              key={value}
-              isActive={filters.length > 0}
-              onClick={onClick}
+            <PlainText
+              fontSize="min(1em, 4vw)"
+              color={isUserPage ? "blue1" : "yellow1"}
             >
-              <Text color={isUserPage ? "blue1" : "yellow1"}>
-                #{value}
-              </Text>
-            </PlainButton>
-          })}
+              {key?.substring(0, 4)}
+            </PlainText>
+          </PlainButton>
+          {filters
+            .filter((f) => f.value !== "one" && f.value !== "ai")
+            .map((filter) => {
+              const { value } = filter;
+              const onClick = () => {
+                const newFilters = filters.filter(
+                  (item) => item.value !== value
+                );
+                setFilters(newFilters);
+                if (newFilters.length === 0) {
+                  setFilterMode(DefaultFilterMode);
+                }
+              };
+              return (
+                <PlainButton
+                  key={value}
+                  isActive={filters.length > 0}
+                  onClick={onClick}
+                >
+                  <Text color={isUserPage ? "blue1" : "yellow1"}>#{value}</Text>
+                </PlainButton>
+              );
+            })}
         </Box>
-      </Box>
-      <Box margin={{ top: '16px' }}>
-        {isLoading &&
-          <Box align={'center'}>
-            <Spinner color={'spinner'} />
+        <Box direction={"row"} alignSelf="center" alignContent="around">
+          <PlainButton
+            isActive={filterMode === "hashtag"}
+            onClick={() => {
+              if (filters.find((item) => item.value === "one")) {
+                const newFilters = filters.filter(
+                  (item) => item.value !== "one"
+                );
+                setFilters(newFilters);
+              } else {
+                setFilters([
+                  ...filters,
+                  {
+                    type: "location",
+                    value: "one",
+                  },
+                ]);
+                setFilterMode("hashtag");
+              }
+            }}
+          >
+            <PlainText fontSize="min(1em, 4vw)">#one</PlainText>
+          </PlainButton>
+          <PlainButton
+            isActive={filterMode === "hashtag"}
+            onClick={() => {
+              if (filters.find((item) => item.value === "ai")) {
+                const newFilters = filters.filter(
+                  (item) => item.value !== "ai"
+                );
+                setFilters(newFilters);
+              } else {
+                setFilters([
+                  ...filters,
+                  {
+                    type: "location",
+                    value: "ai",
+                  },
+                ]);
+                setFilterMode("hashtag");
+              }
+            }}
+          >
+            <PlainText fontSize="min(1em, 4vw)">#ai</PlainText>
+          </PlainButton>
+          <PlainButton style={{ padding: '2px'}}>
+            <PlainText fontSize="min(1em, 4vw)">
+              <StarOutlined />
+            </PlainText>
+          </PlainButton>
+        </Box>
+      </div>
+      <Box>
+        {isLoading && (
+          <Box align={"center"}>
+            <Spinner color={"spinner"} />
           </Box>
-        }
-        {!isLoading && actions.length === 0 &&
-          <Box align={'center'}>
+        )}
+        {!isLoading && actions.length === 0 && (
+          <Box align={"center"}>
             <Text>No actions found</Text>
           </Box>
-        }
+        )}
         {actions.map((action, index) => (
           <UserAction
             userId={key}
@@ -325,6 +420,6 @@ export const UserPage = (props: { id: string }) => {
           />
         ))}
       </Box>
-    </Box>
+    </UserPageBox>
   );
 };
