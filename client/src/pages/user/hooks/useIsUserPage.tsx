bf32da8ccae605f02faf8697bd99b0787eb6e098
potvik <yuriy@harmony.one>
2024-02-23 00:42:13 +0300

    Moved all fetch hooks to separate files

---
+++ client/src/pages/user/hooks/useIsUserPage.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import { useUserContext } from "../../../context";
+import { isSameAddress } from "../../../utils/user";
+
+
+export const useIsUserPage = () => {
+    const { pageOwnerAddress, wallet } = useUserContext();
+    const [isUserPage, setIsUserPage] = useState(false);
+
+    useEffect(() => {
+        if (wallet?.address && pageOwnerAddress) {
+            setIsUserPage(
+                isSameAddress(wallet.address.substring(2), pageOwnerAddress)
+            );
+        }
+    }, [wallet?.address, pageOwnerAddress]);
+
+    return isUserPage;
+}
