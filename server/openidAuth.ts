9efc892189be5068003bcde9629839572656c59b
Rika <15hatoyar@gmail.com>
2024-02-22 18:29:09 -0800

    Add CORS support

--- server/openidAuth.ts
+++ server/openidAuth.ts
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 require('dotenv').config();
 
