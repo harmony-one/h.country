d622fe2ae95dd46623fe23b47bf8768a951d05d0
Rika <15hatoyar@gmail.com>
2024-02-22 17:28:55 -0800

    mod localhost->branch deploy

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -16,7 +16,7 @@ root.render(
   domain={domain as string}
   clientId={clientId as string}
   authorizationParams={{
-    redirect_uri: "http://localhost:3000/"
+    redirect_uri: "http://oauth.hcountry.pages.dev"
   }}
 >
   <App />
